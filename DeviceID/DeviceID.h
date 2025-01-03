25908a9eec087288cd2d691fcc086b45be8ae4a4
Tamás Balla <tamasballa99@gmail.com>
2025-01-03 18:08:31 +0100

    Initial Commit

---
+++ DeviceID/DeviceID.h
@@ -0,0 +1,18 @@
+//
+//  DeviceID.h
+//  DeviceID
+//
+//  Created by Tamás Balla on 2025. 01. 03..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceID.
+FOUNDATION_EXPORT double DeviceIDVersionNumber;
+
+//! Project version string for DeviceID.
+FOUNDATION_EXPORT const unsigned char DeviceIDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceID/PublicHeader.h>
+
+
